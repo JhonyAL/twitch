bb0de8956c23e579da4462103bd8f73c80e22402
JhonyAL <jhonykl666@gmail.com>
2022-11-24 12:00:33 -0300

    first-commit

--- src/Component/Header/Right/index.jsx
+++ src/Component/Header/Right/index.jsx
@@ -1,7 +1,13 @@
+import { CgCrown } from 'react-icons/cg'
+import { FiUser } from 'react-icons/fi'
+
 export default function Right() {
     return (
         <div className="headerRight">
-            
+            <CgCrown className='crowIcon' size={20}/>
+            <button className='btnSignIn'>Entrar</button>
+            <button className='btnSignUp'>Cadastrar-se</button>
+            <FiUser className='userIcon'/>
         </div>
     )
 }
